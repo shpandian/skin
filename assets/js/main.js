2149ed8dbb481f28249e94d5a6d56f0447e13735
Ian McBurnie <imcburnie@ebay.com>
2015-11-11 14:04:15 -0800

    Built skin-ebay from commit 29d5cab on branch v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,20 +70,18 @@ var dialogWidget = function() {
 };
 
 module.exports = dialogWidget;
-},{"jquery":5}],2:[function(require,module,exports){
+},{"jquery":4}],2:[function(require,module,exports){
 'use strict';
 var $ = require('jquery');
-//var navigationWidget = require('./navigationWidget');
 var dialogWidget = require('./dialogWidget');
 var menuWidget = require('./menuWidget');
 
 $(function () {
-    //navigationWidget();
     dialogWidget();
     menuWidget();
 });
 
-},{"./dialogWidget":1,"./menuWidget":3,"jquery":5}],3:[function(require,module,exports){
+},{"./dialogWidget":1,"./menuWidget":3,"jquery":4}],3:[function(require,module,exports){
 'use strict';
 var $ = require('jquery');
 
@@ -105,98 +103,7 @@ var menuWidget = function() {
 
 module.exports = menuWidget;
 
-},{"jquery":5}],4:[function(require,module,exports){
-'use strict';
-var $ = require('jquery');
-
-var NavigationWidget = function () {
-    var HEADER_HEIGHT = 100;
-    var docked = false;
-    var $navigation = $('.left-nav');
-    var $sectionLevelNavItems = $('.left-nav > ul > li');
-    var sections;
-
-    function dockNavigation() {
-        docked = true;
-        $navigation.addClass('docked');
-    }
-
-    function releaseDock() {
-        docked = false;
-        $navigation.removeClass('docked');
-    }
-
-    function isScrollBelowHeader(currentPosition) {
-        return currentPosition > HEADER_HEIGHT;
-    }
-
-    function updateHighlightedSection(scrollTop) {
-        var i, currentSectionId;
-
-        function isElementInViewport($element) {
-            var elementHeight = $element.height();
-            return ($element.offset().top + elementHeight) > (scrollTop);
-        }
-
-        for (i = 0; i < sections.length; i++) {
-            if (isElementInViewport($('#' + sections[i].id))) {
-                $sectionLevelNavItems.removeClass('active');
-                currentSectionId = sections[i].id;
-                $('[href="#' + currentSectionId + '"]').parent().addClass('active');
-                break;
-            }
-        }
-    }
-
-    function scrollHandler() {
-        var currentPosition = $(window).scrollTop();
-
-        if (isScrollBelowHeader(currentPosition)) {
-            dockNavigation();
-        } else {
-            releaseDock();
-        }
-
-        updateHighlightedSection(currentPosition);
-    }
-
-    function findSections() {
-        return $('section').map(function (index, element) {
-            return {
-                id: element.id,
-                offsetTop: element.offsetTop
-            };
-        }).sort(function (a, b) {
-            if (a === b) {
-                return 0;
-              } else if (a.offsetTop > b.offsetTop) {
-                return 1;
-              } else {
-                return -1;
-              }
-        });
-    }
-
-    (function initialize() {
-        sections = findSections();
-        $(window).scroll(scrollHandler);
-    })();
-
-
-    return {
-        isDocked: function () {
-            return docked;
-        },
-        findSections: findSections,
-        isScrollBelowHeader: isScrollBelowHeader,
-        dockNavigation: dockNavigation,
-        releaseDock: releaseDock
-    };
-};
-
-module.exports = NavigationWidget;
-
-},{"jquery":5}],5:[function(require,module,exports){
+},{"jquery":4}],4:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v2.1.4
  * http://jquery.com/
@@ -9408,4 +9315,4 @@ return jQuery;
 
 }));
 
-},{}]},{},[1,2,3,4]);
+},{}]},{},[1,2,3]);
