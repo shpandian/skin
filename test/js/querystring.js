918693ec3d2153e90d6d33fc5708089505f7b26c
Ian McBurnie <imcburnie@ebay.com>
2016-09-01 11:23:41 -0700

    Built skin-ebay from commit 058912f on branch v2

--- test/js/querystring.js
+++ test/js/querystring.js
@@ -4,17 +4,17 @@ window.setTimeout(function() {
     var queryString = window.location.search;
     var h1 = document.getElementsByTagName('h1')[0];
 
-    if (queryString.indexOf('skin-large=on') != -1 ) {
+    if (h1 && queryString.indexOf('skin-large=on') != -1 ) {
         classList.push('skin-large');
         h1.innerText += " - skin-large";
         document.getElementById('skin-large').checked = true;
     }
 
-    if (queryString.indexOf('skin-legacy=on') != -1 ) {
+    if (h1 && queryString.indexOf('skin-legacy=on') != -1 ) {
         classList.push('skin-legacy');
         h1.innerText += " - skin-legacy";
         document.getElementById('skin-legacy').checked = true;
     }
 
     document.getElementsByTagName('body')[0].className = classList.join(' ');
-}, 16);
+}, 32);
